5677c917fe11602a0341ca1bf19732701e48bb0e
Kyle Kauma <kylekauma@gmail.com>
2022-08-30 11:07:29 -0500

    removed API key from OpenWeather component

--- components/OpenWeather.tsx
+++ components/OpenWeather.tsx
@@ -2,7 +2,7 @@ export default function OpenWeather() {
   const callAPI = async () => {
     try {
       const res = await fetch(
-        "https://api.openweathermap.org/data/3.0/onecall?lat=37.4636&lon=122.4286&appid=16178873b704d73031b4a9f794adeef7"
+        "https://api.openweathermap.org/data/3.0/onecall?lat=37.4636&lon=122.4286&appid={}"
       );
       const data = await res.json();
       console.log(data);
